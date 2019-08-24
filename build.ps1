e64afefe7cb9a184557602db15d24ede3b7a1c73
Alena Hrynkevich <alena@appveyor.com>
2019-08-24 12:22:16 -0700

    Create build.ps1

---
+++ build.ps1
@@ -0,0 +1,5 @@
+(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/appveyor/secure-file/master/install.ps1')
+iex ((New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/appveyor/secure-file/master/install.ps1'))
+cmd /c appveyor-tools\secure-file -decrypt my-secret.txt.enc -secret env:my_secret
+$originalValue = (Get-Content my-secret.txt)
+if($originalValue -ne 'Hello, world!') { throw "Decrypted string is not equal to the original one!"; }
